d89fe6b2e118acf65483f996c63591bc0ca64433
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-12-02 10:13:49 +0000

    up first eigenvalue application

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -20,7 +20,7 @@ inline
 po::options_description
 makeOptions()
 {
-    po::options_description gridoptions( "Grid options" );
+    po::options_description gridoptions( "Eigen options" );
     gridoptions.add_options()
     ( "hsize", po::value<double>()->default_value( 0.1 ), "mesh size" )
     ( "kappa", po::value<double>()->default_value( 1 ), "coefficient" )
@@ -31,7 +31,7 @@ makeOptions()
 }
 
 template<int Dim>
-class Grid
+class Eigen
 :
 public Simget
 {
@@ -51,85 +51,73 @@ public:
     typedef boost::shared_ptr<space_type> space_ptrtype;
     typedef typename space_type::element_type element_type;
     typedef Exporter<mesh_type> export_type;
-    
+
     /**
      * Constructor
      */
-    Grid()
-    :
-    super(),
-    M_backend( backend_type::build( this->vm() ) ),
-    meshSize( this->vm()["hsize"].template as<double>() ),
-    shape( this->vm()["shape"].template as<std::string>() ),
-    eigen( SolverEigen<value_type>::build( this->vm() ) )
-    {
-    }
-    
+    Eigen()
+        :
+        super(),
+        M_backend( backend_type::build( this->vm() ) ),
+        meshSize( this->vm()["hsize"].template as<double>() ),
+        eigen( SolverEigen<value_type>::build( this->vm() ) )
+        {
+        }
+
     void run();
 private:
-    
+
     backend_ptrtype M_backend;
     double meshSize;
     std::string shape;
     std::vector<int> flags;
     boost::shared_ptr<SolverEigen<value_type> > eigen;
-}; // Grid
+}; // Eigen
 
-template<int Dim> const uint16_type Grid<Dim>::Order;
+template<int Dim> const uint16_type Eigen<Dim>::Order;
 
 template<int Dim>
 void
-Grid<Dim>::run()
+Eigen<Dim>::run()
 {
     std::cout << "------------------------------------------------------------\n";
-    std::cout << "Execute Grid<" << Dim << ">\n";
-    Environment::changeRepository( boost::format( "doc/manual/%1%/%2%-%3%/P%4%/h_%5%/" )
+    std::cout << "Execute Eigen<" << Dim << ">\n";
+    Environment::changeRepository( boost::format( "po/%1%/%2%D-P%3%/h_%4%/" )
                                   % this->about().appName()
-                                  % shape
                                   % Dim
                                   % Order
                                   % meshSize );
-    
-#if 0
-    GeoTool::Node x1( 0,0 );
-    GeoTool::Node x2( 1,1 );
-    GeoTool::Rectangle Omega( meshSize,"Omega",x1,x2 );
-    Omega.setMarker( _type="line",_name="Paroi",_markerAll=true );
-    Omega.setMarker( _type="surface",_name="Omega",_markerAll=true );
-    
-    auto mesh = Omega.createMesh<mesh_type>( "omega_"+ mesh_type::shape_type::name() );
-#endif
 
-    auto mesh = loadMesh(_mesh = new mesh_type, _physical_are_elementary_regions = true);
+    auto mesh = loadMesh(_mesh = new mesh_type );
 
     auto Xh = space_type::New( mesh );
     auto u = Xh->element();
     auto v = Xh->element();
-    
-    value_type kappa = this->vm()["kappa"].template as<double>();
-    value_type nu = this->vm()["nu"].template as<double>();
-    
+
     auto a = form2( _test=Xh, _trial=Xh);
     a = integrate( elements( mesh ), gradt(u)*trans(grad(v)) );
-    
+
     auto b = form2( _test=Xh, _trial=Xh);
     b = integrate( elements(mesh), idt( u )*id( v ) );
-    
-    
-    int maxit = this->vm()["solvereigen-maxiter"].template as<int>();
-    int tol = this->vm()["solvereigen-tol"].template as<double>();
-    
-    int nev = this->vm()["solvereigen-nev"].template as<int>();
-    
-    int ncv = this->vm()["solvereigen-ncv"].template as<int>();;
-    
+
+
+    int maxit = option(_name="solvereigen-maxiter").template as<int>();
+    int tol = option(_name="solvereigen-tol").template as<double>();
+
+    int nev = option(_name="solvereigen-nev").template as<int>();
+
+    int ncv = option(_name="solvereigen-ncv").template as<int>();;
+
     double eigen_real, eigen_imag;
-    
+
     SolverEigen<double>::eigenmodes_type modes;
-    
-    std::cout << "nev= " << nev <<std::endl;
-    std::cout << "ncv= " << ncv <<std::endl;
-    
+
+    if ( Environment::worldComm().isMasterRank() )
+    {
+        std::cout << "nev= " << nev <<std::endl;
+        std::cout << "ncv= " << ncv <<std::endl;
+    }
+
     modes=
     eigs( _matrixA=a.matrixPtr(),
          _matrixB=b.matrixPtr(),
@@ -138,13 +126,13 @@ Grid<Dim>::run()
          _transform=SINVERT,
          _spectrum=SMALLEST_MAGNITUDE,
          _verbose = true );
-    
+
     auto femodes = std::vector<decltype( Xh->element() )>( modes.size(), Xh->element() );
-    
+
     if ( !modes.empty() )
     {
         LOG(INFO) << "eigenvalue " << 0 << " = (" << modes.begin()->second.get<0>() << "," <<  modes.begin()->second.get<1>() << ")\n";
-        
+
         int i = 0;
         BOOST_FOREACH( auto mode, modes )
         {
@@ -152,25 +140,25 @@ Grid<Dim>::run()
             femodes[i++] = *mode.second.get<2>();
         }
     }
-    
+
     auto exporter =  export_type::New( this->vm(),
                                       ( boost::format( "%1%-%2%-%3%" )
                                        % this->about().appName()
                                        % shape
                                        % Dim ).str() ) ;
-    
+
     if ( exporter->doExport() )
     {
         LOG(INFO) << "exportResults starts\n";
-        
+
         exporter->step( 0 )->setMesh( mesh );
-        
+
         int i = 0;
         BOOST_FOREACH( auto mode, femodes )
         {
             exporter->step( 0 )->add( ( boost::format( "mode-%1%" ) % i++ ).str(), mode );
         }
-        
+
         exporter->save();
         LOG(INFO) << "exportResults done\n";
     }
@@ -181,16 +169,16 @@ int
 main( int argc, char** argv )
 {
     using namespace Feel;
-    
+
     Environment env( _argc=argc, _argv=argv,
                     _desc=makeOptions(),
                     _about=about(_name="po_eigen",
-                                 _author="Abdoulaye Samake",
-                                 _email="samakeablo@gmail.com") );
-    
+                                 _author="",
+                                 _email="") );
+
     Application app;
-    
-    app.add( new Grid<2>() );
+
+    app.add( new Eigen<2>() );
     app.run();
 }
 
