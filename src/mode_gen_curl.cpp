c0bfebb84965f2e01007ca5c6ba4774cf972413b
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-12-19 23:53:46 +0000

    update  - added vectorial space for visualisation of velocity eigen modes  - added divergence term for stabilisation

--- src/mode_gen_curl.cpp
+++ src/mode_gen_curl.cpp
@@ -36,11 +36,16 @@ public:
     typedef FunctionSpace<mesh_type, basis_type> space_type;
     typedef boost::shared_ptr<space_type> space_ptrtype;
     typedef typename space_type::element_type element_type;
+
+    typedef bases<Lagrange<Order,Vectorial>> vbasis_type;
+    typedef FunctionSpace<mesh_type, vbasis_type> vspace_type;
+    //typedef typename vspace_type::element_type element_type;
+
     typedef Exporter<mesh_type> export_type;
-    
+
     void run();
 private:
-    
+
 }; // EigenProblem
 
 template<int Dim> const uint16_type EigenProblem<Dim>::Order;
@@ -49,16 +54,20 @@ template<int Dim>
 void
 EigenProblem<Dim>::run()
 {
-    std::cout << "------------------------------------------------------------\n";
-    std::cout << "Execute EigenProblem<" << Dim << ">\n";
-    
+    if ( Environment::worldComm().isMasterRank() )
+    {
+        std::cout << "------------------------------------------------------------\n";
+        std::cout << "Execute EigenProblem<" << Dim << ">\n";
+    }
     Environment::changeRepository( boost::format( "po/%1%/%2%D-P%3%/" )
                                   % this->about().appName()
                                   % Dim
                                   % Order );
-    
+
     auto mesh = loadMesh(_mesh = new mesh_type );
-    
+
+    auto Vh = vspace_type::New( mesh );
+    auto W = Vh->element();
     auto Xh = space_type::New( mesh );
     auto U = Xh->element();
     auto u1 = U.template element<0>();
@@ -68,90 +77,85 @@ EigenProblem<Dim>::run()
     auto v1 = V.template element<0>();
     auto v2 = V.template element<1>();
     auto v3 = V.template element<2>();
-    
+
     auto l = form1( _test=Xh );
     auto a = form2( _test=Xh, _trial=Xh);
     a = integrate( elements( mesh ), (dyt(u3)-dzt(u2)) * (dy(v3)-dz(v2)) );
     a+= integrate( elements( mesh ), (dzt(u1)-dxt(u3)) * (dz(v1)-dx(v3)) );
     a+= integrate( elements( mesh ), (dxt(u2)-dyt(u1)) * (dx(v2)-dy(v1)) );
-    
+    a+= integrate( elements( mesh ), (dxt(u1)+dyt(u2)+dzt(u3)) *(dx(u1)+dy(u2)+dz(u3)));
+
     a += on(_range=markedfaces(mesh, 1), _rhs=l, _element=u3, _expr=cst(0.));
     a += on(_range=markedfaces(mesh, 2), _rhs=l, _element=u3, _expr=cst(0.));
     a += on(_range=markedfaces(mesh, 3), _rhs=l, _element=u1, _expr=cst(0.));
     a += on(_range=markedfaces(mesh, 3), _rhs=l, _element=u2, _expr=cst(0.));
-    
+
     auto b = form2( _test=Xh, _trial=Xh);
     b = integrate( elements(mesh), idt( u1 )*id( v1 ) );
     b += integrate( elements(mesh), idt( u2 )*id( v2 ) );
     b += integrate( elements(mesh), idt( u3 )*id( v3 ) );
-    
+
     int nev = option(_name="solvereigen.nev").template as<int>();
-    
+
     int ncv = option(_name="solvereigen.ncv").template as<int>();
-    
+
     double eigen_real, eigen_imag;
-    
+
     SolverEigen<double>::eigenmodes_type modes;
-    
+
     if ( Environment::worldComm().isMasterRank() )
     {
         std::cout << "nev= " << nev <<std::endl;
         std::cout << "ncv= " << ncv <<std::endl;
     }
-    
+
     modes=
     eigs( _matrixA=a.matrixPtr(),
          _matrixB=b.matrixPtr(),
          _nev=nev,
          _ncv=ncv,
-         //_transform=SINVERT,
-         //_spectrum=SMALLEST_MAGNITUDE,
+         _transform=SINVERT,
+         _spectrum=SMALLEST_MAGNITUDE,
          _verbose = true );
-    
+
+    auto e =  exporter( _mesh=mesh );
     auto femodes = std::vector<decltype( Xh->element() )>( modes.size(), Xh->element() );
-    
+    auto vfemodes = std::vector<decltype( Vh->element() )>( modes.size(), Vh->element() );
     if ( !modes.empty() )
     {
         LOG(INFO) << "eigenvalue " << 0 << " = (" << modes.begin()->second.get<0>() << "," <<  modes.begin()->second.get<1>() << ")\n";
-        
+
         int i = 0;
         for( auto const& mode : modes )
         {
             std::cout << " -- eigenvalue " << i << " = (" << mode.second.get<0>() << "," <<  mode.second.get<1>() << ")\n";
-            femodes[i++] = *mode.second.get<2>();
+            femodes[i] = *mode.second.get<2>();
+            W = vf::project(_space=Vh, _range=elements(mesh),
+                            _expr=vec(idv(femodes[i].template element<0>()),
+                                      idv(femodes[i].template element<1>()),
+                                      idv(femodes[i].template element<2>()) ) );
+            e->add( ( boost::format( "mode-%1%" ) % i ).str(), W );
+            ++i;
         }
-    }
-    
-    auto e =  exporter( _mesh=mesh );
-    
-    if ( e->doExport() )
-    {
-        LOG(INFO) << "exportResults starts\n";
-        int i = 0;
-        for( auto const& mode: femodes )
-        {
-            e->add( ( boost::format( "mode-%1%" ) % i++ ).str(), mode );
-        }
-        
         e->save();
-        LOG(INFO) << "exportResults done\n";
     }
-    
+
+
 }
 
 int
 main( int argc, char** argv )
 {
     using namespace Feel;
-    
+
     Environment env( _argc=argc, _argv=argv,
                     _desc=feel_options(),
                     _about=about(_name="po_mode_gen_curl",
                                  _author="",
                                  _email="") );
-    
+
     Application app;
-    
+
     app.add( new EigenProblem<3>() );
     app.run();
 }
