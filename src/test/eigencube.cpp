c35b5f4fbb45bec386437943694e6652b41ffdb6
Romain Hild <romain.hild@me.com>
2014-11-12 11:59:31 +0100

    fix Bellout eigenmodes

--- src/test/eigencube.cpp
+++ src/test/eigencube.cpp
@@ -20,17 +20,17 @@ int main(int argc, char** argv)
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<3> > );
 
     auto Vh = Pchv<2>( mesh );
-    auto u = Vh->element(0);
+    auto u = Vh->element();
 
-    auto a1 = vec(cst(0.0113),cst(0.),-cst(0.0219));
-    auto a2 = vec(-cst(0.0004),cst(0.),cst(0.0219));
-    auto a3 = vec(cst(0.0004),cst(0.),-cst(0.0219));
-    auto a4 = vec(-cst(0.0113),cst(0.),cst(0.0219));
+    auto a1 = vec(cst(0.0113),cst(0.),cst(-0.0219));
+    auto a2 = vec(cst(-0.0004),cst(0.),cst(0.0219));
+    auto a3 = vec(cst(0.0004),cst(0.),cst(-0.0219));
+    auto a4 = vec(cst(-0.0113),cst(0.),cst(0.0219));
 
-    auto b1 = vec(cst(0.0175),-cst(0.0089),cst(0.0111));
-    auto b2 = vec(-cst(0.0175),cst(0.0320),-cst(0.0111));
-    auto b3 = vec(-cst(0.0175),-cst(0.0089),cst(0.0034));
-    auto b4 = vec(-cst(0.0175),cst(0.0320),-cst(0.0034));
+    auto b1 = vec(cst(0.0175),cst(-0.0089),cst(0.0111));
+    auto b2 = vec(cst(-0.0175),cst(0.0320),cst(-0.0111));
+    auto b3 = vec(cst(-0.0175),cst(-0.0089),cst(0.0034));
+    auto b4 = vec(cst(-0.0175),cst(0.0320),cst(-0.0034));
 
     auto k1 = vec(cst(2.),cst(3.),cst(1.));
     auto k2 = vec(cst(2.),cst(3.),cst(-1.));
@@ -45,6 +45,12 @@ int main(int argc, char** argv)
                  _expr=a3*cos(inner(P(),k3))-b3*sin(inner(P(),k3)) );
     u += project(_range=elements(mesh), _space=Vh,
                  _expr=a4*cos(inner(P(),k4))-b4*sin(inner(P(),k4)) );
+
+    auto err = normL2(_range=elements(mesh), _expr=curlv(u)+std::sqrt(14)*idv(u) );
+    auto nor = normL2(_range=elements(mesh), _expr=inner(idv(u),N()) );
+
+    if ( Environment::isMasterRank() )
+	std::cout << "n : " << nor << "\ne : " << err << std::endl;
 
     auto e = exporter( mesh );
     e->add( "u", u);
