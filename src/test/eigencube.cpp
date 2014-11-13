440813d88e76cf0eb6a5f6373122c8e959031b06
romainhild <romain.hild@me.com>
2014-11-13 14:27:49 +0000

    add the imaginary part of the eigenfunctions

--- src/test/eigencube.cpp
+++ src/test/eigencube.cpp
@@ -20,40 +20,57 @@ int main(int argc, char** argv)
     auto mesh = loadMesh( _mesh=new Mesh<Simplex<3> > );
 
     auto Vh = Pchv<2>( mesh );
-    auto u = Vh->element();
+
+    if ( Environment::isMasterRank() )
+        std::cout << "[dof] number of dof: " << Vh->nDof() << std::endl;
+
+    auto ur = Vh->element("0");
+    auto ui = Vh->element("0");
 
     auto a1 = vec(cst(0.0113),cst(0.),cst(-0.0219));
     auto a2 = vec(cst(-0.0004),cst(0.),cst(0.0219));
     auto a3 = vec(cst(0.0004),cst(0.),cst(-0.0219));
     auto a4 = vec(cst(-0.0113),cst(0.),cst(0.0219));
+    decltype(a1) a[4] = {a1,a2,a3,a4};
 
     auto b1 = vec(cst(0.0175),cst(-0.0089),cst(0.0111));
     auto b2 = vec(cst(-0.0175),cst(0.0320),cst(-0.0111));
-    auto b3 = vec(cst(-0.0175),cst(-0.0089),cst(0.0034));
+    auto b3 = vec(cst(-0.0175),cst(0.0089),cst(-0.0034));
     auto b4 = vec(cst(-0.0175),cst(0.0320),cst(-0.0034));
+    decltype(b1) b[4] = {b1,b2,b3,b4};
 
     auto k1 = vec(cst(2.),cst(3.),cst(1.));
     auto k2 = vec(cst(2.),cst(3.),cst(-1.));
     auto k3 = vec(cst(2.),cst(-3.),cst(1.));
     auto k4 = vec(cst(-2.),cst(3.),cst(1.));
+    decltype(k1) k[4] = {k1,k2,k3,k4};
 
-    u = project(_range=elements(mesh), _space=Vh,
-                _expr=a1*cos(inner(P(),k1))-b1*sin(inner(P(),k1)) );
-    u += project(_range=elements(mesh), _space=Vh,
-                 _expr=a2*cos(inner(P(),k2))-b2*sin(inner(P(),k2)) );
-    u += project(_range=elements(mesh), _space=Vh,
-                 _expr=a3*cos(inner(P(),k3))-b3*sin(inner(P(),k3)) );
-    u += project(_range=elements(mesh), _space=Vh,
-                 _expr=a4*cos(inner(P(),k4))-b4*sin(inner(P(),k4)) );
 
-    auto err = normL2(_range=elements(mesh), _expr=curlv(u)+std::sqrt(14)*idv(u) );
-    auto nor = normL2(_range=elements(mesh), _expr=inner(idv(u),N()) );
+    for( int i = 0; i < 4; i++ ) {
+        ur += project(_range=elements(mesh), _space=Vh,
+                      _expr=a[i]*cos(inner(P(),k[i])) - b[i]*sin(inner(P(),k[i])) );
+        ui += project(_range=elements(mesh), _space=Vh,
+                      _expr=a[i]*sin(inner(P(),k[i])) + b[i]*cos(inner(P(),k[i])) );
+    }
 
-    if ( Environment::isMasterRank() )
-	std::cout << "n : " << nor << "\ne : " << err << std::endl;
+
+    auto errR = normL2(_range=elements(mesh), _expr=curlv(ur)-std::sqrt(14)*idv(ur) );
+    auto errI = normL2(_range=elements(mesh), _expr=curlv(ui)-std::sqrt(14)*idv(ui) );
+    auto norR = normL2(_range=elements(mesh), _expr=inner(idv(ur),N()) );
+    auto norI = normL2(_range=elements(mesh), _expr=inner(idv(ui),N()) );
+    auto divR = normL2(_range=elements(mesh), _expr=divv(ur) );
+    auto divI = normL2(_range=elements(mesh), _expr=divv(ui) );
+
+    if ( Environment::isMasterRank() ) {
+        std::cout << "nR : " << norR << "\nnI : " << norI << std::endl;
+        std::cout << "eR : " << errR << "\neI : " << errI << std::endl;
+        std::cout << "dR : " << divR << "\ndI : " << divI << std::endl;
+    }
+
 
     auto e = exporter( mesh );
-    e->add( "u", u);
+    e->add( "Re(u)", ur);
+    e->add( "Im(u)", ui);
     e->save();
 
 }
