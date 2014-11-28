46702fc4beb54122f0f1a4c0c7e0360056d00f01
romainhild <romain.hild@me.com>
2014-11-28 15:46:28 +0100

    Add a formulation in which u is in h(div) and w is in h(curl)

--- src/eigenmixed.cpp
+++ src/eigenmixed.cpp
@@ -16,9 +16,8 @@ public Simget
 public:
   typedef Mesh<Simplex<3 > > mesh_type;
 
-  typedef bases<RaviartThomas<0>, Nedelec<0,NedelecKind::NED1> > basis_type;
+  typedef bases<RaviartThomas<0>, Nedelec<0,NedelecKind::NED1>, Lagrange<2,Scalar > > basis_type;
   typedef FunctionSpace<mesh_type, basis_type > space_type;
-  typedef boost::shared_ptr<space_type> space_ptrtype;
 
   void run();
 };
@@ -33,24 +32,66 @@ EigenProblem::run()
     std::cout << "Execute EigenProblem\n";
   }
 
+  auto epsilon = doption("parameters.epsilon");
+  auto gamma = doption("parameters.gamma");
+
   auto mesh = loadMesh( new mesh_type );
 
   auto Xh = space_type::New( mesh );
+
   auto U = Xh->element();
-  auto w = U.element<0>();
+
+#ifdef PO_UHCURL
+  // if u is in h(curl) and w in h(div)
   auto u = U.element<1>();
+  auto w = U.element<0>();
+  auto p = U.element<2>();
 
   auto a = form2( _test=Xh, _trial=Xh );
-  a = integrate( _range=elements(mesh),
-  		 _expr=inner(idt(w),id(w))
-  		 - inner(curlt(u),id(w))
-  		 + inner(idt(w),id(u)) );
+  a = integrate( elements(mesh),
+		 - trans(curlt(u))*id(w) - trans(idt(u))*trans(grad(p))
+  		 + inner(idt(w),id(w)) + trans(idt(w))*curl(u)
+		 + gradt(p)*id(u)
+		 + epsilon*inner(idt(u),id(u))
+		 + gamma*inner(idt(p),id(p))
+  		 );
+
+  a += integrate( boundaryfaces( mesh ),
+       		        trans(idt(u))*cross(id(w),N()) );
+
+#else
+  // if u is in h(div) and w in h(curl)
+  auto u = U.element<0>();
+  auto w = U.element<1>();
+  auto p = U.element<2>();
+
+  auto a = form2( _test=Xh, _trial=Xh );
+  a = integrate( elements(mesh),
+		 - trans(idt(u))*curl(w) + divt(u)*id(p)
+  		 + inner(idt(w),id(w)) + trans(curlt(w))*id(u)
+		 - idt(p)*div(u)
+		 + epsilon*inner(idt(u),id(u))
+		 + gamma*inner(idt(p),id(p))
+  		 );
+
+  a += integrate( boundaryfaces( mesh ),
+		  -trans(idt(w))*cross(id(u),N()) );
+
+#endif
 
   auto b = form2( _test=Xh, _trial=Xh );
-  b = integrate( _range=elements(mesh), _expr=inner(idt(u),id(u)) );
+  b = integrate( elements(mesh),
+  		           inner(idt(u),id(u))
+		              //  + gamma*inner(idt(w),id(w))
+		              //  + gamma*inner(idt(p),id(p))
+  		               );
 
   auto l = form1( _test=Xh );
-  a += on(_range=boundaryfaces(mesh), _rhs=l, _element=w, _expr=zero<3,1>() );
+  if( boption("bcVec") )
+      a += on(_range=boundaryfaces(mesh), _rhs=l, _element=w, _expr=zero<3,1>() );
+  else
+      a += on(_range=boundaryfaces(mesh), _rhs=l, _element=w, _expr=cst(0.) );
+
 
   auto modes = veigs( _formA=a, _formB=b );
 
@@ -81,22 +122,33 @@ EigenProblem::run()
     }
 
     e->add( (boost::format("mode-%1%")%i).str(), g);
+    i++;
   }
 
   e->save();
 }
 
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
+  po::options_description appoptions( "" );
+	appoptions.add_options()
+    ( "bcVec", po::value<bool>()->default_value( true ), "Dirichlet is vectorial" );
+
   Environment env( _argc=argc, _argv=argv,
-		   _desc=feel_options(),
-		   _about=about(_name="po_eigenmixt",
+		   _desc=appoptions,
+		   _desc_lib=feel_options(),
+		   _about=about(_name=
+#ifdef PO_UHCURL
+				"po_eigenmixed_curl",
+#else
+				"po_eigenmixed_div",
+#endif
 				_author="Romain Hild",
 				_email="romain.hild@plasticomnium.com") );
 
   Application app;
-  
+
   app.add( new EigenProblem() );
   app.run();
 }
