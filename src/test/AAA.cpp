800029a4d135fe41b2c1e9ccc42f8ded3c9d5ccd
bsurowiec <benjamin.surowiec@plasticomnium.com>
2015-06-04 13:08:49 +0200

    up AAA

--- src/test/AAA.cpp
+++ src/test/AAA.cpp
@@ -23,12 +23,57 @@ int main(int argc, char** argv)
                                    _author="BS-RH",
                                    _email="benjamin.surowiec@plasticomnium.com"));
 
-    auto mesh = loadMesh(new meshdim);
+    auto Th = loadMesh(new meshdim);
 
+    auto XVh2 = FEspaceP1P0::New( Th );
 
+    auto U = XVh2->element();
+    auto u = U.element<0>();
+    auto p = U.element<1>();
 
+    auto V = XVh2->element();
+    auto v = V.element<0>();
+    auto q = V.element<1>();
 
+    auto a = form2( _trial=XVh2, _test=XVh2 );
 
+    a = integrate( _range=element(Th),
+                   _expr=inner(gradt(u),grad(v)) );
 
+    a += integrate( _range=element(Th),
+                    _expr=idt(u)*id(q) );
 
+    a += integrate( _range=element(Th),
+                    _expr=id(v)*idt(p) );
+
+    auto f = expr(soption("functions.f"));
+    auto h_s = soption("functions.h");
+    auto vars = Symbols{ "x", "y", "radius", "meanvel" };
+    auto h_e = parse( h_s, vars );
+    auto alpha0 = expr( h_e, vars );
+    alpha0.setParameterValues( {
+            { "radius", 0.5 },
+                { "meanvel", 1 } } );
+
+    auto d = integrate(markedfaces(mesh, 1), -alpha0).evaluate()(0,0);
+    d += integrate(markedfaces(mesh, 2), alpha0).evaluate()(0,0);
+    std::cout << " alpha0 : " << h_s << "\nint : " << d << std::endl;
+
+    auto l = form1( _test=XVh2);
+
+    l = integrate( _range=markedfaces(Th, 1),
+                   _expr=-alpha0*id(v) );
+
+    l = integrate( _range=markedfaces(Th, 2),
+                   _expr=alpha0*id(v) );
+
+    a.solve( _rhs=l,
+             _solution=U );
+
+    auto e = exporter(Th);
+    e->add("u", u);
+    e->add("p", p);
+    e->save();
+
+    return 0;
 }
