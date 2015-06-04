65a9de2d3cfd871e607e58cf3f860e8951f3a8f6
Vincent Huber <vincent.huber@cemosis.fr>
2015-06-04 12:44:50 +0200

    use options to make it simpler

--- src/test/AAA.cpp
+++ src/test/AAA.cpp
@@ -7,24 +7,36 @@
 using namespace Feel;
 using namespace Feel::vf;
 
-typedef Mesh<Simplex<3> > mesh_type;
+typedef Mesh<Simplex<FEELPP_DIM> > meshT;
 typedef Lagrange<1,Scalar> baseP1;
 typedef Lagrange<0,Scalar> baseP0;
 typedef bases<baseP1, baseP0 > baseP1P0;
-typedef FunctionSpace<mesh_type, bases < baseP1> > FEspaceP1;
-typedef FunctionSpace<mesh_type, baseP1P0> FEspaceP1P0;
+typedef FunctionSpace<meshT, bases < baseP1> > FEspaceP1;
+typedef FunctionSpace<meshT, baseP1P0> FEspaceP1P0;
 
 
+inline
+  po::options_description
+makeOptions()
+{
+  po::options_description EXPRoptions( "AAA options" );
+  EXPRoptions.add_options()
+    ( "meanlevel", po::value<double>()->default_value( 1 ), "Mean Level Of ...;" )
+    ( "radius", po::value<double>()->default_value( 0.5 ), "Radius Of ..." )
+    ;
+  return EXPRoptions;
+}
 
 int main(int argc, char** argv)
 {
     Environment env( _argc=argc, _argv=argv,
-                     _desc=feel_options(),
+                     //_desc=feel_options(),
+                     _desc=makeOptions(), //feel_options are added, available via --help-lib
                      _about=about( _name="AAA",
                                    _author="BS-RH",
                                    _email="benjamin.surowiec@plasticomnium.com"));
 
-    auto mesh = loadMesh(new mesh_type);
+    auto mesh = loadMesh(new meshT);
 
     auto XVh2 = FEspaceP1P0::New( mesh );
 
@@ -48,17 +60,18 @@ int main(int argc, char** argv)
                     _expr=id(v)*idt(p) );
 
     auto f      = expr(soption("functions.f"));
-    auto h_s    = soption("functions.h");
-    auto vars   = Symbols{ "x", "y", "radius", "meanvel" };
-    auto h_e    = parse( h_s, vars );
-    auto alpha0 = expr( h_e, vars );
-    alpha0.setParameterValues( {
-            { "radius", 0.5 },
-                { "meanvel", 1 } } );
+    auto alpha0      = expr(soption("functions.h"));
+    //auto h_s    = soption("functions.h");
+    //auto vars   = Symbols{ "x", "y", "radius", "meanvel" };
+    //auto h_e    = parse( h_s, vars );
+    //auto alpha0 = expr( h_e, vars );
+    //alpha0.setParameterValues( {
+    //        { "radius", 0.5 },
+    //            { "meanvel", 1 } } );
 
     auto d = integrate(markedfaces(mesh, 1), -alpha0).evaluate()(0,0);
     d += integrate(markedfaces(mesh, 2), alpha0).evaluate()(0,0);
-    std::cout << " alpha0 : " << h_s << "\nint : " << d << std::endl;
+    std::cout << " alpha0 : " << alpha0 << "\nint : " << d << std::endl;
 
     auto l = form1( _test=XVh2);
 
