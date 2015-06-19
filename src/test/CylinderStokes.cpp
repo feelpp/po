b1739f086549faa631711f07bf8c53c0b8cd618c
bsurowiec <benjamin.surowiec@plasticomnium.com>
2015-06-19 13:38:10 +0200

    from darcy to neumann problem

--- src/test/CylinderStokes.cpp
+++ src/test/CylinderStokes.cpp
@@ -14,10 +14,10 @@ using namespace Feel::vf;
 #define FEELPP_DIM 3
 
 typedef Mesh<Simplex<FEELPP_DIM>> MeshDim;
+typedef Lagrange<2,Scalar> BaseP2;
 typedef Lagrange<1,Scalar> BaseP1;
-typedef RaviartThomas<0> BaseRT0;
-typedef bases<BaseRT0,BaseP1> BaseRT0P1;
-typedef FunctionSpace<MeshDim,BaseRT0P1> FESpaceRT0P1;
+typedef bases<BaseP2,BaseP1> BaseP2P1;
+typedef FunctionSpace<MeshDim,BaseP2P1> FESpaceP2P1;
 
 // EXTENDED OPTION
 
@@ -50,60 +50,55 @@ int main(int argc, char** argv)
     auto Th = loadMesh( new MeshDim );
 
     // FINITE ELEMENT SPACE
-    auto Xh = FESpaceRT0P1::New( Th );
+    auto Xh = FESpaceP2P1::New( Th );
 
     // UNKOWN VARIABLE
-    auto Q2 = Xh->element( );
-    auto q2 = Q2.element<0>( );
-    auto psi2 = Q2.element<1>( );
+    auto PSI2 = Xh->element( );
+    auto psi2 = PSI2.element<0>( );
+    auto prpsi2 = PSI2.element<1>( );
 
     // TEST FUNCTION
     auto V = Xh->element( );
     auto v = V.element<0>( );
-    auto phiv = V.element<1>( );
+    auto prv = V.element<1>( );
 
-    // BOUNDARY CONDITION : RIGHT HAND SIDE : F, ALPHA0 AND ALPHA2
+    // RIGHT HAND SIDE : F AND ALPHA2
     auto f = expr( soption( "functions.f" ) );
     auto alpha2 = expr( soption( "functions.g" ) );
-    //auto alpha0 = expr( soption( "functions.h" ) );
 
     // LINEAR FORM
-    auto l2 = from1( _test=Xh );
+    auto l2 = form1( _test=Xh );
 
     l2 = integrate( _range=elements( Th ),
                     _expr=trans( f )*id( v ) );
 
-    // BILINEAR FORM -- SEEKING SOLUTION IN HDIV0
-    auto adarcy = form2( _trial=Xh,
-                         _test=Xh );
+    l2 += integrate( _range=markedfaces( Th, 1 ),
+                     _expr=-alpha2*id( v ) );
 
-    adarcy = integrate ( _range=elements( Th ),
-                         _expr=-idt( q2 )*id( v ) );
+    l2 += integrate( _range=markedfaces( Th, 2 ),
+                     _expr=alpha2*id( v ) );
 
-    adarcy += integrate ( _range=elements( Th ),
-                          _expr=-idt( psi2 )*div( v ) );
+    // BILINEAR FORM
+    auto a2 = form2( _trial=Xh,
+                     _test=Xh );
 
-    adarcy += integrate ( _range=elements( Th ),
-                          _expr=divt( q2 )*id( phiv ) );
+    a2 = integrate ( _range=elements( Th ),
+                     _expr=inner(gradt( psi2 ),gradd( v )) );
 
-    adarcy += on ( _range=markedfaces( Th, 1 ), rhs=l2,
-                   _element=q2, _expr=-alpha2 );
+    a2 += integrate ( _range=elements( Th ),
+                      _expr=idt( psi2 )*id( prv ) );
 
-    adarcy += on ( _range=markedfaces( Th, 2 ), rhs=l2,
-                   _element=q2, _expr=alpha2 );
-
-    adarcy += on ( _range=markedfaces( Th, 3 ), rhs=l2,
-                   _element=q2, _expr=cst(0.) );
+    a2 += integrate ( _range=elements( Th ),
+                      _expr=id( v )*idt( prpsi2 ) );
 
     // SOLVING PROBLEM
-    adarcy.solve( _rhs=l2,
-                  _solution=Q2 );
+    a2.solve( _rhs=l2,
+              _solution=PSI2 );
 
     // RESULT STORAGE
-    auto export = exporter( Th );
-    export->add( "q2", q2 );
-    export->add( "psi2", psi2 );
-    export->save( );
+    auto e = exporter( Th );
+    e->add( "psi2", psi2 );
+    e->save( );
 
     // END OF MAIN PROGRAM
     return 0;
