b2db23c3a1aee2b26590b7c9aeefd2263fc399ff
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-10-14 21:12:37 +0200

    get feelpp.version.cmake

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -9,6 +9,7 @@ fi
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 mkdir -p tools/scripts/buildkite/
+mkdir -p cmake/modules/
 
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock feelpp/feelpp-libs \
        cat /usr/local/share/feelpp/scripts/release.sh | dos2unix > tools/scripts/buildkite/release.sh
@@ -16,15 +17,14 @@ docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock feelpp/feelpp-l
        cat /usr/local/share/feelpp/scripts/list.sh | dos2unix > tools/scripts/buildkite/list.sh
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock feelpp/feelpp-libs \
        cat /usr/local/share/feelpp/scripts/common.sh | dos2unix > tools/scripts/buildkite/common.sh
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock feelpp/feelpp-libs \
+       cat /usr/local/share//feelpp/feel/cmake/modules/feelpp.version.cmake | dos2unix > cmake/modules/feelpp.version.cmake
 
 
 chmod u+x tools/scripts/buildkite/release.sh
 chmod u+x tools/scripts/buildkite/list.sh
 chmod u+x tools/scripts/buildkite/common.sh
 
-export FEELPP_DIR=/usr/local
-source tools/scripts/buildkite/common.sh
-echo "cmake : ${FEELPP_CMAKE_DIR}"
 tools/scripts/buildkite/release.sh -t ${TARGET} -b ${BRANCH} -- ${PROJECT}
 
 
